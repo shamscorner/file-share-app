0eafd2e9d07daa6111e8af1c317bd684f3c69d81
Shamim Hossain <hossains159@gmail.com>
2022-06-26 22:29:00 +0600

    refactor: move helpers to plugins

--- composables/index.ts
+++
@@ -1,8 +0,0 @@
-export const getRandomIntInclusive = (
-  minArg: number,
-  maxArg: number
-): number => {
-  const min = Math.ceil(minArg);
-  const max = Math.floor(maxArg);
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
